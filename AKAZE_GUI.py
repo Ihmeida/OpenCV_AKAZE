7097e5a0b50313e9cbf7ac504b34b87141d96332
abc95007 <32200491+abc95007@users.noreply.github.com>
2020-10-29 23:28:07 +0800

    image show in canvas

--- AKAZE_GUI.py
+++ AKAZE_GUI.py
@@ -7,12 +7,10 @@
 import cv2
 from PIL import ImageTk 
 from PIL import Image  
-
 import tkinter as tk
 from tkinter import filedialog as fd 
 import os
 from datetime import datetime
-
 
 #%%
 def kaze_match(im1_path, im2_path, number:int=20, isShow:bool=False):
@@ -43,18 +41,13 @@ def kaze_match(im1_path, im2_path, number:int=20, isShow:bool=False):
 
     # cv2.drawMatchesKnn expects list of lists as matches.
     data = cv2.drawMatchesKnn(im1, kps1, im2, kps2, good[0:number], None, flags=2)
-    if(isShow):
-        cv2.imshow("AKAZE matching", data)
-        cv2.waitKey(0) 
+
     return data
 
 #%%
 def AkazeClick(event):
-    
     global canvas
     global img
-    #global labelImg
-    print("AkazeClick click")
     if not os.path.isfile(image1):
         info.set("Please read image1")
         return
@@ -63,19 +56,16 @@ def AkazeClick(event):
         return
 
     data = kaze_match(image1, image2, isShow=False)
-    fileName = "AKAZE_"+ datetime.now().strftime('%Y-%m-%d_%H_%M_%S') + ".bmp"
-    fileName = os.path.join(os.path.abspath("./"), fileName)
-    
-    #print(fileName)
-    info.set("compare: \n{0} \n{1} \nSave:{2}".format(image1, image2, fileName))
-    label.update()
-
-    Image.fromarray(data).save(fileName)
-    img =  ImageTk.PhotoImage(image=Image.fromarray(data), master=window)
-
-    #labelImg["image"] = img
-    #labelImg["text"] = "cleck ed"
-    #labelImg.update()
+    #fileName = "AKAZE_"+ datetime.now().strftime('%Y-%m-%d_%H_%M_%S') + ".bmp"
+    #fileName = os.path.join(os.path.abspath("./"), fileName)
+    if(False):
+        pass
+        #info.set("compare: \n{0} \n{1} \nSave:{2}".format(image1, image2, fileName))
+        #Image.fromarray(data).save(fileName)
+        #label.update()
+    else:
+        info.set("compare: \n{0} \n{1}".format(image1, image2))
+    img =  ImageTk.PhotoImage(image=Image.fromarray(data), master=root)
 
     canvas.create_image(0, 0, anchor="nw", image=img)
     canvas.update() 
@@ -91,39 +81,31 @@ def CallFile2():
     info.set("Read: {}".format(image2))
 
 #%%
-
 image1 = ""
 image2 = ""
 im3 = ""
 img = ""
 
-window = tk.Tk()
-window.title("RAD AKAZE")
-window.geometry("800x800")
+root = tk.Tk()
+root.title("RAD AKAZE")
+root.geometry("900x800")
 info = tk.StringVar()
-info.set("Hello! please read image")
+info.set("Please read image!")
 
-btnCallFile1 = tk.Button(window, text="Read image1", command=CallFile1)
+btnCallFile1 = tk.Button(root, text="Read image1", command=CallFile1)
 btnCallFile1.pack()
 
-btnCallFile2 = tk.Button(window, text="Read image2", command=CallFile2)
+btnCallFile2 = tk.Button(root, text="Read image2", command=CallFile2)
 btnCallFile2.pack()
 
-btn = tk.Button(window, text="AKAZE click")
+btn = tk.Button(root, text="AKAZE click")
 btn.pack()
 btn.bind("<Button-1>", AkazeClick)
 
-label = tk.Label(window, textvariable=info, bg="light green")
+label = tk.Label(root, textvariable=info, bg="light green")
 label.pack()
 
-#labelImg = tk.Label(window, text = "test")
-#labelImg.pack()
-
-# TODO 後續待修改如何由 click 更新至 canvas 上
-canvas = tk.Canvas(window, width=800, height=700,  background="white")
-#canvas.create_image(0, 0, anchor="nw",  background="white")
+canvas = tk.Canvas(root, width=900, height=700,  background="white")
 canvas.pack()
-window.mainloop()
 
-#%%
-os.path.abspath("./")
+root.mainloop()
--- README.md
+++ README.md
@@ -6,12 +6,7 @@ Using OpenCV (python) AKAZE compare image feature.
 1. Click **Read image1**
 2. Click **Read image2**
 3. Click **AKAZE click**
-4. Save bmp to reference path
 
-![image](https://github.com/abc95007/OpenCV_AKAZE/blob/master/Image/AKAZE.png?raw=true)
 
 # Result
-![image](https://images2.imgbox.com/ee/53/KmQVmVBV_o.jpg)
-
-# Exe link
-https://drive.google.com/file/d/1QYq8PHz-Jzfhg3CSsPutzNDF5zhOew0Q/view?usp=sharing
+![image](https://upload.cc/i1/2020/10/29/HBZ3zo.png)
